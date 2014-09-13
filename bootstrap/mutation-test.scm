5487031858eef24d769472eb075c02ef214547e9
tca <theconartist@gmail.com>
2014-09-13 17:06:15 -0500

    mutation analysis bootstrap

---
+++ bootstrap/mutation-test.scm
@@ -0,0 +1,134 @@
+;; Pattern matching
+(define (any p l)
+  (if (null? l)
+      #f
+      (if (p (car l))
+          #t
+          (any p (cdr l)))))
+
+(define (conj a b) (lambda (x) (and (a x) (b x))))
+(define (disj a b) (lambda (x) (or (a x) (b x))))
+
+(define (pattern? p e)
+  (cond ((null? p) (null? e))
+        ((equal? p '_) #t)
+        ((symbol? p) (and (symbol? e) (equal? p e)))
+        ((procedure? p) (p e))
+        (else (and (pair? e)
+                   (pattern? (car p) (car e))
+                   (pattern? (cdr p) (cdr e))))))
+
+(define (primitive-value? expr)
+  (or (null? expr)
+      (symbol? expr)
+      (number? expr)
+      (string? expr)
+      (boolean? expr)
+      (pattern? '(quote _) expr)))
+
+;; Mutable cells
+
+(define (make-cell v) (cons v '()))
+(define (cell-value c) (car c))
+(define (set-cell! c v) (set-car! c v))
+
+
+;; mutation boxing
+
+(define (mutable! mutable-vars v)
+  (let* ((mutables (cell-value mutable-vars)))
+    (if (member v mutables)
+        v
+        (begin (set-cell! mutable-vars (cons v mutables))
+               v))))
+
+(define (mut-collect mvars e)
+  (cond ((pattern? '(begin . _) e)
+         `(begin . ,(map (lambda (se) (mut-collect mvars se)) (cdr e))))
+        ((pattern? '(if _ _ _) e)
+         (let ((b (cadr e)) (thn (caddr e)) (els (cadddr e)))
+           (mut-collect mvars b)
+           (mut-collect mvars thn)
+           (mut-collect mvars els)))
+        ((pattern? '(lambda _ _) e)
+         (let ((vs (if (list? (cadr e)) (cadr e) (list (cadr e))))
+               (m (caddr e)))
+           (let ((inner-mut-vars (make-cell '())))
+             (mut-collect inner-mut-vars  m)
+             ;; dont bubble up vars bound by this lambda
+             (map (lambda (v) (if (member v vs)
+                                   '()
+                                  (mutable! mvars v))) (cell-value inner-mut-vars)))))
+        ((pattern? '(set! _ _) e)
+         (mutable! mvars (cadr e))
+         (mut-collect mvars (caddr e)))
+        ((pattern? '(_ . _) e)
+         (mut-collect mvars (car e))
+         (map (lambda (a) (mut-collect mvars a)) (cdr e)))
+        (else '()))
+  e)
+
+(define (mut-conv mvars replace e)
+  (cond ((symbol? e) (if (member e replace) `(car ,e) e))
+        ((primitive-value? e) e)
+        ((pattern? '(begin . _) e)
+         `(begin . ,(map (lambda (se) (mut-conv mvars replace se)) (cdr e))))
+        ((pattern? '(if _ _ _) e)
+         (mut-collect mvars e)
+         (let ((b (cadr e)) (thn (caddr e)) (els (cadddr e)))
+           `(if ,(mut-conv mvars replace b)
+                ,(mut-conv mvars replace thn)
+                ,(mut-conv mvars replace els))))
+        
+        ((pattern? '(lambda _ _) e)
+         (let* ((vs (cadr e))
+                (vs-normal (if (list? vs) vs (list vs)))
+                (m (caddr e)))
+           (let* ((inner-mut-vars (make-cell '()))
+                  (_ (mut-collect inner-mut-vars  m))
+                  (not-shadowed (set-remove replace vs-normal))
+                  ;; dont replace vars shadowed by this lambda that aren't mutable
+                  (conv-body (mut-conv inner-mut-vars (append not-shadowed
+                                                              (cell-value inner-mut-vars)) m))
+                  (boxed-vars (map (lambda (v) `(cons ,v '()))
+                                   (set-intersect vs-normal (cell-value inner-mut-vars)))))
+             (if (equal? '() boxed-vars)
+                 `(lambda ,vs ,conv-body)
+                 `(lambda ,vs
+                    ((lambda ,vs ,conv-body) . ,boxed-vars))))))
+        ((pattern? '(set! _ _) e)
+         (mut-collect mvars e)
+         `(set-car! ,(cadr e) ,(mut-conv mvars replace (caddr e))))
+        ((pattern? '(_ . _) e)
+         (mut-collect mvars e)
+         `(,(mut-conv mvars replace (car e)) . ,(map (lambda (a) (mut-conv mvars replace a)) (cdr e))))
+        (else (error "dont know how to mut-conv" e))))
+
+(define in-code1
+  '((lambda ()
+      (begin
+        (lambda (a)
+          (begin (display a) (set! a 2) (display a) (set! a 3) (display a)))
+        1))))
+
+(define out-code1
+  '((lambda ()
+      (begin
+        (lambda (a)
+          ((lambda (a)
+             (begin
+               (display (car a))
+               (set-car! a 2)
+               (display (car a))
+               (set-car! a 3)
+               (display (car a))))
+           (cons a '())))
+        1))))
+
+(let ((mut-form (mut-conv (make-cell '()) '() in-code1)))
+  (display 'mutation-analysis)
+  (newline)
+  (display (if (equal? mut-form out-code1) "PASSED" "FAILED"))
+  (newline)
+  (display mut-form)
+  (newline))
