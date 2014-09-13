8d07d7560e1d7f9da861bdb9a14ee4cf11fcbf65
tca <theconartist@gmail.com>
2014-09-12 19:35:58 -0500

    test tostring

---
+++ testtostring.scm
@@ -0,0 +1,21 @@
+(define (tostring2 obj)
+  (cond
+   ((string? obj) obj)
+   ((char? obj) (char->string obj))
+   ((boolean? obj) (if obj "#t" "#f"))
+   ((procedure? obj) "#<procedure>")
+   ((number? obj) (number->string obj))
+   ((symbol? obj) (symbol->string obj))
+   ((null? obj) "()")
+   ((list? obj) (foldl string-append "(" (append
+                                          (list (foldl (lambda (m c) (string-append m " " (tostring c)))
+                                                       (tostring (car obj))
+                                                       (cdr obj)))
+                                          
+                                          (cons ")" '()))))
+   ((pair? obj) (foldl string-append "(" (cons (tostring2 (car obj))
+                                               (cons " . "
+                                                     (cons (tostring2 (cdr obj))
+                                                           (cons ")" '()))))))))
+
+(display (tostring2 '(a b c)))
